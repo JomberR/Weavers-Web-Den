9211b773dc755221633136ae801f34894be65f79
JomberR <60245648+JomberR@users.noreply.github.com>
2020-02-27 04:23:24 -0600

    Update test.js

--- GateWaysToTheMultiverse/scenes/test.js
+++ GateWaysToTheMultiverse/scenes/test.js
@@ -7,6 +7,7 @@ class Test extends Phaser.Scene{
     preload(){
         //Image used for our tileset.
         this.load.image("tile", "./GateWaysToTheMultiverse/scenes/assets/images/basicTile.png");
+        this.load.image("player", "./GateWaysToTheMultiverse/scenes/assets/images/basicPlayer.png")
     }
 
     create(){
@@ -36,6 +37,35 @@ class Test extends Phaser.Scene{
 
         //0, 0 is where we place the layer. We can use this to make rooms!!!
         var layer = map.createStaticLayer(0, tiles, 128, 128);
+
+        //THIS WILL BE MIGRATED TO A SEPARATE FILE
+        //Create the player
+        var player = this.add.sprite(160, 160, "player");
+
+        //Create the keys in order to control the player
+        var key6 = this.input.keyboard.addKey("NUMPAD_SIX");
+        key6.on("down", function(event){
+            player.setPosition(player.x + 64, player.y);
+        });
+
+        var key4 = this.input.keyboard.addKey("NUMPAD_FOUR");
+        key4.on("down", function(event){
+            player.setPosition(player.x - 64, player.y);
+        });
+
+        var key2 = this.input.keyboard.addKey("NUMPAD_TWO");
+        key2.on("down", function(event){
+            player.setPosition(player.x, player.y + 64);
+        });
+
+        var key8 = this.input.keyboard.addKey("NUMPAD_EIGHT");
+        key8.on("down", function(event){
+            player.setPosition(player.x, player.y - 64);
+        });
+    }
+
+    update(){
+
     }
 
 }
